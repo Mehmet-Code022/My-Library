1239534161793f9577c4e5df9ee1420220700752
Mehmet-Code022 <mehmetyagiz911@gmail.com>
2026-02-22 17:53:27 +0300

    Create main.h

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MEHMET_H
+#define MEHMET_H
+
+#include <string>
+
+void TextAnimation(std::string Text, float speed_ms, int error_rate, int remove_speed = 100);
+void Backspace(int count, int speed);
+
+#endif
