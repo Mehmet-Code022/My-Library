11c26d29225b615846378af6335b55b644deeab1
Mehmet-Code022 <mehmetyagiz911@gmail.com>
2026-02-23 15:10:13 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,9 +1,10 @@
 #ifndef MEHMET_H
 #define MEHMET_H
-
 #include <string>
 
 void TextAnimation(std::string Text, float speed_ms, int error_rate, int remove_speed = 100);
 void Backspace(int count, int speed);
+void ShowProgressBar(int len_of_bar, int WaitTime); 
+void AutoScript(std::string text, int AutoSpeed,int text_speed, int error_possibility);
 
 #endif
