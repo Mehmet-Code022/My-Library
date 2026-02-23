03aea4e0d577ce73204943ab86db15ff8e6f63fa
Mehmet-Code022 <mehmetyagiz911@gmail.com>
2026-02-23 15:11:21 +0300

    Update main.cpp
    ->AutoScript is the most advanced and user-friendly function in the library. It automatically generates a progress bar based on the input text and then displays the text with a typewriter effect. It streamlines the entire process.
    
    Parameters:
    
        text: The main string to be displayed on the screen.
    
        AutoSpeed: The fill speed of the progress bar (in milliseconds).
    
        text_speed: The delay between characters while typing the text.
    
        error_possibility: The probability of making a typo (between 0 and 100).

--- main.cpp
+++ main.cpp
@@ -4,7 +4,15 @@
 #include <thread>
 #include <cstdlib>
 
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define RESET   "\033[0m"
+
 using namespace std;
+bool On_Write = false;
+int Textlen = 0;
+int time_of_prograss;
 
 void Backspace(int count, int speed) {
     for (int i = 0; i < count; i++) {
@@ -13,7 +21,8 @@ void Backspace(int count, int speed) {
     }
 }
 
-void TextAnimation(string Text, float speed_ms, int error_rate, int remove_speed) {
+void TextAnimation(string Text, float speed_ms, int error_rate, int remove_speed) { 
+    Textlen = Text.length();
     static bool isSeeded = false;
     if (!isSeeded) {
         srand(time(0));
@@ -23,9 +32,14 @@ void TextAnimation(string Text, float speed_ms, int error_rate, int remove_speed
     float original_speed_ms = speed_ms;
     int Error_Waiting = 450;
 
+    cout << "|" << flush; this_thread::sleep_for(chrono::milliseconds(400));
+    cout << "\b \b" << flush; this_thread::sleep_for(chrono::milliseconds(400));
     for (int i = 0; i < (int)Text.length(); i++) {
+        cout << "\a";
+        time_of_prograss = ((i + 1) * 100) / Textlen;
+
         speed_ms = original_speed_ms;
-        float RandSpeed = 0.2f + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (2.0f - 0.2f))); // 2.0 ile 0.2 arasi rastgele sayi
+        float RandSpeed = 0.2f + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (2.0f - 0.2f))); 
         speed_ms = speed_ms * RandSpeed;
 
         if (Text[i] == '.' || Text[i] == ',' || Text[i] == '!' || Text[i] == '?') {
@@ -35,18 +49,44 @@ void TextAnimation(string Text, float speed_ms, int error_rate, int remove_speed
         int RandNumber = rand() % 101;
         // Hata yapma mantığı
         if ((RandNumber < error_rate && Text[i] != ' ') && (i + 1 < (int)Text.length())) {
+            cout << RED;
+            On_Write = true;
             cout << (char)(Text[i] + 2) << Text[i + 1] << flush;
             this_thread::sleep_for(chrono::milliseconds(Error_Waiting));
             
             Backspace(2, remove_speed);
-            
+            cout << RESET;
             cout << Text[i] << Text[i + 1] << flush;
             i++;
         } else {
+            cout << RESET;
             cout << Text[i] << flush;
+            On_Write = true;
         }
         
-        this_thread::sleep_for(chrono::milliseconds(static_cast<int>(speed_ms))); // static_cast<int> ile float degerimizi integera cevirdik ki hata almayalim
+        this_thread::sleep_for(chrono::milliseconds(static_cast<int>(speed_ms))); 
     }
-    cout << endl; // Satırı bitir
+    cout << endl; 
+}
+
+
+void ShowProgressBar(int len_of_bar, int WaitTime) {
+    cout << YELLOW;
+    cout << "[" << flush; 
+    for (int i = 0; i < len_of_bar; i++) {
+        cout << "#" << flush;
+        this_thread::sleep_for(chrono::milliseconds(WaitTime));
+    }
+    cout << "] Yukleme Tamamlandi!" << endl;
+    cout << RESET;
+}
+
+void AutoScript(string text, int AutoSpeed,int text_speed, int error_possibility){
+    int len_of_text = text.length();
+    int a = len_of_text / 2;
+    ShowProgressBar(a , AutoSpeed);
+
+    cout << endl;
+
+    TextAnimation(text, text_speed, error_possibility);
 }
